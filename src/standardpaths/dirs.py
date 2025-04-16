a46b756fc3b0f953e530f449a49286876dda0c80
Matthew J. Milner <matterhorn103@proton.me>
2025-04-16 03:51:28 +0200

    Basic support for app_name and caching via init

--- src/standardpaths/dirs.py
+++ src/standardpaths/dirs.py
@@ -39,7 +39,7 @@ class StandardPaths:
             _data = _xdg_data or _win_app_data or "~/AppData/Roaming"
             _local_data = _win_local_app_data or "~/AppData/Local"
             _config = _xdg_config or _local_data
-            _state = _xdg_state or _local_data + "/State"
+            _state = _xdg_state or _local_data # /<APPNAME>/State
             _app = _win_programs or "~/AppData/Roaming/Microsoft/Windows/Start Menu/Programs"
             _cache = _xdg_cache or  _local_data + "/cache"
             _runtime = _xdg_runtime or _win_tmp or _local_data + "/Temp"
@@ -55,7 +55,7 @@ class StandardPaths:
         case "darwin":
             _data = _xdg_data or "~/Library/Application Support"
             _config = _xdg_config or "~/Library/Preferences"
-            _state = _xdg_state or "~/Library/Preferences/State" # Should really be ~/Library/Preferences/<APPNAME>/State
+            _state = _xdg_state or "~/Library/Preferences" # /<APPNAME>/State
             _app = "/Applications"
             _cache = _xdg_cache or "~/Library/Caches"
             _runtime = _xdg_runtime or "~/Library/Application Support"
@@ -73,7 +73,7 @@ class StandardPaths:
             # This is fine as it aligns with QStandardPaths and platformdirs
             _data = _xdg_data or "~/Library/Application Support"
             _config = _xdg_config or "~/Library/Preferences"
-            _state = _xdg_state or "~/Library/Preferences/State"  # Mirror macOS
+            _state = _xdg_state or "~/Library/Preferences"  # Mirror macOS
             _app = _xdg_default_app  # This should maybe be unsupported on mobile
             _cache = _xdg_cache or "~/Library/Caches"
             _runtime = _xdg_runtime or "~/Library/Caches"  # With QStandardPaths this is unsupported
@@ -142,33 +142,29 @@ class StandardPaths:
         return Path.home()
     
     @classmethod
-    def data(cls, local=False):
+    def data(cls, app_name: str = None, local=False):
+        app_name = app_name if app_name else ""
         if local and sys.platform == "win32":
-            if isinstance(cls._local_data, str):
-                cls._local_data = Path(cls._local_data).expanduser()
-            return cls._local_data
+            return Path(cls._local_data, app_name).expanduser()
         else:
-            if isinstance(cls._data, str):
-                cls._data = Path(cls._data).expanduser()
-            return cls._data
+            return Path(cls._data, app_name).expanduser()
     
     @classmethod
-    def config(cls):
-        if isinstance(cls._config, str):
-            cls._config = Path(cls._config).expanduser()
-        return cls._config
+    def config(cls, app_name: str = None):
+        app_name = app_name if app_name else ""
+        return Path(cls._config, app_name).expanduser()
 
     @classmethod
-    def state(cls):
-        if isinstance(cls._state, str):
-            cls._state = Path(cls._state).expanduser()
-        return cls._state
+    def state(cls, app_name: str = None):
+        app_name = app_name if app_name else ""
+        if sys.platform in ["win32", "darwin", "ios"]:
+            return Path(cls._state, app_name, "State").expanduser()
+        else:
+            return Path(cls._state, app_name).expanduser()
 
     @classmethod
     def app(cls):
-        if isinstance(cls._app, str):
-            cls._app = Path(cls._app).expanduser()
-        return cls._app
+        return Path(cls._app).expanduser()
     
     #@classmethod
     #def program_files(cls):
@@ -180,36 +176,42 @@ class StandardPaths:
     #        raise RuntimeError("Program Files exists only on Windows!")
 
     @classmethod
-    def cache(cls):
-        if isinstance(cls._cache, str):
-            cls._cache = Path(cls._cache).expanduser()
-        return cls._cache
+    def cache(cls, app_name: str = None):
+        app_name = app_name if app_name else ""
+        return Path(cls._cache, app_name).expanduser()
 
     @classmethod
     def runtime(cls):
-        if isinstance(cls._runtime, str):
-            cls._runtime = Path(cls._runtime).expanduser()
-        return cls._runtime
+        return Path(cls._runtime).expanduser()
 
     @classmethod
-    def data_dirs(cls, include_home=False, local=False):
+    def data_dirs(cls, app_name: str = None, include_home=False, local=False):
+        app_name = app_name if app_name else ""
         # Follow XDG spec and don't include user data home unless requested
         # Would be more convenient if `include_home=True` by default though...
-        if cls._data_dirs and isinstance(cls._data_dirs[0], str):
-            cls._data_dirs = [Path(p).expanduser() for p in cls._data_dirs]
+        dirs = [Path(p, app_name).expanduser() for p in cls._data_dirs]
         if include_home:
-            return [cls.data(local=local)] + cls._data_dirs
+            return [cls.data(app_name=app_name, local=local)] + dirs
         else:
-            return cls._data_dirs
+            return dirs
 
     @classmethod
-    def config_dirs(cls, include_home=False):
+    def config_dirs(cls, app_name: str = None, include_home=False):
+        app_name = app_name if app_name else ""
         # Follow XDG spec and don't include user config home unless requested
         # Would be more convenient if `include_home=True` by default though...
-        if cls._config_dirs and isinstance(cls._config_dirs[0], str):
-            cls._config_dirs = [Path(p).expanduser() for p in cls._config_dirs]
+        dirs = [Path(p, app_name).expanduser() for p in cls._config_dirs]
         if include_home:
-            return [cls.config()] + cls._config_dirs
+            return [cls.config(app_name=app_name)] + dirs
         else:
-            return cls._config_dirs
-
+            return dirs
+        
+    def __init__(self, app_name: str):
+        self.data = self.data(app_name=app_name)
+        self.config = self.config(app_name=app_name)
+        self.state = self.state(app_name=app_name)
+        self.app = self.app()
+        self.cache = self.cache(app_name=app_name)
+        self.runtime = self.runtime()
+        self.data_dirs = self.data_dirs(app_name=app_name)
+        self.config_dirs = self.config_dirs(app_name=app_name)
